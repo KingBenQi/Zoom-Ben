90b21551131652f926f93bacda308671e3c8bed9
Your Name <you@example.com>
2024-04-18 03:07:09 +0800

    remove syntax error in sidebar

--- zoom-ben/components/Sidebar.tsx
+++ zoom-ben/components/Sidebar.tsx
@@ -6,14 +6,7 @@ const Sidebar = () => {
       className="sticky left-0 top-0 flex h-screen w-fit 
   flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]"
     >
-      <div className="flex flex- flex-col gap-6">
-        {[
-          {
-            imgUrl: "/icons/Home.svg",
-            route,
-          },
-        ]}
-      </div>
+      <div className="flex flex- flex-col gap-6">{[]}</div>
     </section>
   );
 };
